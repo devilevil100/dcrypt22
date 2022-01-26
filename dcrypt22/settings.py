142d7e63069eaf8bd6cb7aa4861c7eeea854f028
Keshav Kumar <61359976+devilevil100@users.noreply.github.com>
2022-01-26 13:19:12 +0530

    Update settings.py

--- dcrypt22/settings.py
+++ dcrypt22/settings.py
@@ -13,15 +13,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os.path
 
-STATIC_ROOT = ''
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ( os.path.join('static'), )
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
