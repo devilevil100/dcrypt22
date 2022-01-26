8e85c2168e75e451d2806e5c85ad2b409c06da4d
Keshav Kumar <61359976+devilevil100@users.noreply.github.com>
2022-01-26 13:41:44 +0530

    Update asgi.py

--- dcrypt22/asgi.py
+++ dcrypt22/asgi.py
@@ -1,26 +1,7 @@
-"""
-ASGI config for dcrypt22 project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
+import django
+from channels.routing import get_default_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import users.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcrypt22.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            users.routing.websocket_urlpatterns
-        )
-    ),
-})
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcrypt22.settings')
+django.setup()
+application = get_default_application()
