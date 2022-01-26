421fa3b4cac9c7465361ed9acf8481e618bf2110
Keshav Kumar <61359976+devilevil100@users.noreply.github.com>
2022-01-26 13:22:00 +0530

    Update settings.py

--- dcrypt22/settings.py
+++ dcrypt22/settings.py
@@ -16,7 +16,7 @@ import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
