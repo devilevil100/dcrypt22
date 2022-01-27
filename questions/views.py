b85a15d8ec5174879f8a8185105e4e3dd42badc1
Keshav Kumar <61359976+devilevil100@users.noreply.github.com>
2022-01-28 00:48:50 +0530

    Update views.py

--- questions/views.py
+++ questions/views.py
@@ -9,6 +9,7 @@ from questions.models import Question, CheckQues, CurrentQues
 from django.contrib.auth.hashers import make_password, check_password
 from django.views.decorators.http import require_http_methods
 import datetime
+from ratelimit.decorators import ratelimit
 import requests
 from discord import Webhook, RequestsWebhookAdapter
 
@@ -17,6 +18,8 @@ solveweb = Webhook.from_url("https://discord.com/api/webhooks/936104431710240778
 
 
 # Create your views here.
+
+@ratelimit(key='ip', rate='5/m')
 def quest(request):
     if not request.session.get('name'):
         return redirect('dashboard:login')
@@ -53,6 +56,8 @@ def quest(request):
         request.session.pop('hack')
     return render(request,"question.html", {"question": current, "room": room.roomname, "incor":incorr, "hacc":hacc})
 
+
+@ratelimit(key='ip', rate='5/m')
 @require_http_methods(["POST"])
 def answer(request):
     if not request.session.get('name'):
